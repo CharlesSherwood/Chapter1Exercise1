1fb43be85abb50f60d30e7d20e38a206754f97fb
cks45004 <cks45004@HT-0232-14>
2025-01-17 10:30:32 -0500

    Add project files.

---
+++ chapter1exercise1.cpp
@@ -0,0 +1,26 @@
+/*
+* File name: Chapter1Exersice1.cpp
+Programmer:Charles Sherwood 
+Date:01/17/2025
+Requirments:
+"What the program is supposed to do"
+
+
+
+
+
+*/
+
+#include <iostream>
+//should type before you start programming
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+
+}
+
